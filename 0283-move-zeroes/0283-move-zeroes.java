79380d33869aad50493b5e108c38261481cff9da
YOGESH <j.yogesh8458@gmail.com>
2026-01-22 18:27:49 +0530

    Time: 2 ms (89.65%), Space: 47.9 MB (45.88%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        //in this  problem we will consider two pointer one which travers to  the whole for loop and keep the pos as  0 if we find the element rather then 0 we will move it to front .
+
+        int pos = 0 ;
+
+        for(int i = 0 ; i < nums.length ; i++ ){
+            if(nums[i] != 0 ){
+                nums[pos] = nums[i];
+                pos ++ ;
+            }
+        }
+
+        while(pos < nums.length){
+            nums[pos] = 0 ;
+            pos ++ ;
+        }
+
+
+
+        
+    }
+}
